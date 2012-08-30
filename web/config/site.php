3bc2ad462d819aa09dcd79a33af3f56251541833
Casaval <casaval@gmail.com>
2012-08-30 12:22:12 -0700

    added config file, removed excluder from .gitignore, whoops

---
+++ web/config/site.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'novatel');
+define('DB_PASSWORD', 'H8y^j0Lnen');
+define('DB_DATABASE', 'novatel');
+define('PASSWORD_SALT', 'VB6kbNLP60TfCGakVqTYaEa2T8oJaJJ0B00g1KnChhDRJnjYi8D8wfFsyemhCH31');
