34b91700631d73cca784df22d76a4bb1fdb3a858
Casaval <casaval@gmail.com>
2012-09-12 12:40:07 -0700

    breadcrumb home icon

---
+++ web/blocks/autonav/templates/breadcrumb.php
@@ -0,0 +1,44 @@
+<?php 
+	defined('C5_EXECUTE') or die("Access Denied.");
+	$aBlocks = $controller->generateNav();
+	$c = Page::getCurrentPage();
+	$nh = Loader::helper('navigation');
+	$i = 0;
+	foreach($aBlocks as $ni) {
+		$_c = $ni->getCollectionObject();
+
+		$pageLink = false;
+
+		$target = $ni->getTarget();
+		if ($target != '') {
+			$target = 'target="' . $target . '"';
+		}
+		
+		if ($_c->getCollectionAttributeValue('replace_link_with_first_in_nav')) {
+			$subPage = $_c->getFirstChild();
+			if ($subPage instanceof Page) {
+				$pageLink = $nh->getLinkToCollection($subPage);
+			}
+		}
+		
+		if (!$pageLink) {
+			$pageLink = $ni->getURL();
+		}
+
+		if ($i > 0) {
+			print ' <span class="ccm-autonav-breadcrumb-sep">&gt;</span> ';
+		}
+		if ($c->getCollectionID() == $_c->getCollectionID()) { 
+			echo($ni->getName());
+		} else {
+			$name = $ni->getName();
+			if (strcasecmp($name, "Home") == 0){
+				$name = "<i class='icon-home'></i> ";
+			}
+			echo('<a href="' . $pageLink . '" ' . $target . '>' . $name . '</a>');
+		}	
+		$lastLevel = $thisLevel;
+		$i++;
+	}
+	
+	$thisLevel = 0;
