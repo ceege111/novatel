e6ced915c66a508232454ba08e4dfe0c65814a05
Casaval <casaval@gmail.com>
2012-10-08 13:21:09 -0700

    changed setting on site.php to NOT show the welcome popup by default

--- web/config/site.php
+++ web/config/site.php
@@ -4,3 +4,4 @@ define('DB_USERNAME', 'novatel');
 define('DB_PASSWORD', 'H8y^j0Lnen');
 define('DB_DATABASE', 'novatel');
 define('PASSWORD_SALT', 'VB6kbNLP60TfCGakVqTYaEa2T8oJaJJ0B00g1KnChhDRJnjYi8D8wfFsyemhCH31');
+define('ENABLE_NEWSFLOW_OVERLAY', FALSE);
