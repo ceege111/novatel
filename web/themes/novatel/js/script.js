3344b481c78c09ceeb9fe58e379c1bff5747d14a
Casaval <casaval@gmail.com>
2012-10-15 11:36:57 -0700

    iframe page type auto adjust height

--- web/themes/novatel/js/script.js
+++ web/themes/novatel/js/script.js
@@ -30,10 +30,10 @@ $(document).ready(function(){
 	});
 
 	//iframe content resize
-	var calculatedHeight = $(window).innerHeight() - $(".headerWrap").outerHeight();
+	var calculatedHeight = $(window).innerHeight() - $(".headerWrap").outerHeight(true);
 	$("#iframeContent").attr('height', calculatedHeight);
 	$(window).resize(function(){
-		var calcHeight = $(window).innerHeight() - $(".headerWrap").outerHeight();
+		var calcHeight = $(window).innerHeight() - $(".headerWrap").outerHeight(true);
 		$("#iframeContent").attr('height',calcHeight);
 	});
 
