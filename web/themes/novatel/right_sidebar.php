e4bec7114e633040616149a406fa482eb9011428
Casaval <casaval@gmail.com>
2012-10-23 14:34:49 -0700

    added stying for default page titles

--- web/themes/novatel/right_sidebar.php
+++ web/themes/novatel/right_sidebar.php
@@ -3,19 +3,20 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $this->inc('elements/header.php'); ?>
 
 <header class="pageTitle container">
-	<div class="row banner">
-	    <?php
+    <?php
 	    $a = new Area('Header Banner');
 	    if (($a->getTotalBlocksInArea($c) > 0) || ($c->isEditMode())) {
+	    	echo '<div class="row banner">';
 	        $a->setBlockLimit(1);
 	        $a->display($c);
 	    } else { ?>
-	    	<h1 class="span12">
-				<?php
-					$page = Page::getCurrentPage();
-					echo $page->getCollectionName();
-				?>
-			</h1>
+			<div class="row defaultBanner banner">
+		    	<h1 class="defaultTitle span12">
+					<?php
+						$page = Page::getCurrentPage();
+						echo $page->getCollectionName();
+					?>
+				</h1>
 	    <?php } ?>
 	</div>
 	<div class="subNav area">
