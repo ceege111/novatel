d749fe873eb68f8ea317889b6ae78253a04bab67
Casaval <casaval@gmail.com>
2012-11-02 20:11:11 -0700

    default header h1 css fix, sidebar default wrapper set

--- web/themes/novatel/elements/sidebar_right.php
+++ web/themes/novatel/elements/sidebar_right.php
@@ -4,6 +4,8 @@ defined('C5_EXECUTE') or die("Access Denied.");
 	<section class="rightColumn area">
 		<?php 
 		$as = new Area('RightSidebar');
+		$as->setBlockWrapperStart('<div class="sidebar-block">');
+		$as->setBlockWrapperEnd('</div>');	
 		$as->display($c);
 		?>
 	</section>
