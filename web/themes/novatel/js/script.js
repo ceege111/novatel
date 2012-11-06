10ad579f634456ef6e949ab418ec79b2ce6e97d5
Casaval <casaval@gmail.com>
2012-11-06 15:44:45 -0800

    where to buy

--- web/themes/novatel/js/script.js
+++ web/themes/novatel/js/script.js
@@ -76,10 +76,10 @@ $(document).ready(function(){
 		$(this).change(function(){
 			var region = $(this).val();
 			console.log('changing to region '+region);
-			$('select.country-menu').not('select.country-menu.menu-'+region).each(function(){
+			$('.country-menu').not('.country-menu.menu-'+region).each(function(){
 				$(this).hide();
 			});
-			$('select.country-menu.menu-'+region).fadeIn();
+			$('.country-menu.menu-'+region).fadeIn();
 			$('div.carrier-list').each(function(){
 				$(this).hide();
 			});
