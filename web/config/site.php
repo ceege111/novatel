ee0e520f40927fd732353577460624211ce9f1a8
Casaval <casaval@gmail.com>
2012-12-11 15:29:06 -0800

    advanced permission settings

--- web/config/site.php
+++ web/config/site.php
@@ -5,4 +5,3 @@ define('DB_PASSWORD', 'H8y^j0Lnen');
 define('DB_DATABASE', 'novatel');
 define('PASSWORD_SALT', 'VB6kbNLP60TfCGakVqTYaEa2T8oJaJJ0B00g1KnChhDRJnjYi8D8wfFsyemhCH31');
 define('ENABLE_NEWSFLOW_OVERLAY', FALSE);
-define('PERMISSIONS_MODEL', 'advanced');
