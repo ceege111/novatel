8dec1a3f90800223a651204e7fa972595ffe4348
Casaval <casaval@gmail.com>
2013-01-09 12:57:17 -0800

    debugging product download template

--- web/blocks/autonav/templates/product_downloads.php
+++ web/blocks/autonav/templates/product_downloads.php
@@ -30,7 +30,7 @@
 
 	foreach($aBlocks as $ni) {
 		$_c = $ni->getCollectionObject();
-		if (!$_c->getCollectionAttributeValue('exclude_nav')) {
+		// if (!$_c->getCollectionAttributeValue('exclude_nav')) {
 			
 			
 			// $target = $ni->getTarget();
@@ -111,7 +111,7 @@
 
 			$lastLevel = $thisLevel;
 			$i++;
-		}
+		// }
 	}
 
 	//debug
