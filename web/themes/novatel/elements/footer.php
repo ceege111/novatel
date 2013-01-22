9c978807eef315901a19b24f8d255dc524e40c59
Casaval <casaval@gmail.com>
2013-01-22 15:01:38 -0800

    bitbucket webhook test

--- web/themes/novatel/elements/footer.php
+++ web/themes/novatel/elements/footer.php
@@ -24,7 +24,7 @@
 
 	<!-- google-analytics -->
 	<script>
-		var _gaq=[['_setAccount','UA-PLACEHOLDER-4'],['_trackPageview']];
+		var _gaq=[['_setAccount','UA-PLACEHOLDER-5'],['_trackPageview']];
 		(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
 		g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
 		s.parentNode.insertBefore(g,s)}(document,'script'));
