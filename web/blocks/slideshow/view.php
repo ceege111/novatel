06bfe5ac1ad0077d91281ac6ae33b68f17bd8854
Casaval <casaval@gmail.com>
2013-01-24 14:21:33 -0800

    gridslider customize: controls always visible

--- web/blocks/slideshow/view.php
+++ web/blocks/slideshow/view.php
@@ -48,7 +48,7 @@ $(document).ready(function(){
 		container.append(link);
 		if (key == list.length-1){
 			console.log("starting carousel...");
-			$("#ccm-SlideshowBlock-display<?=intval($bID)?>").gridSlider({autoplay_enable:true,autoplay_interval:4,scroll_axis:'x',loop:true});
+			$("#ccm-SlideshowBlock-display<?=intval($bID)?>").gridSlider({autoplay_enable:true,autoplay_interval:6,scroll_axis:'x',loop:true,ctrl_always_visible:true});
 		}
 	});
 });
