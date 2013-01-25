61e6b07bd4f3023cf4e281579bf4f207d37d2031
Casaval <casaval@gmail.com>
2013-01-24 16:48:08 -0800

    product list truncate short descriptionw

--- web/blocks/page_list/templates/products.php
+++ web/blocks/page_list/templates/products.php
@@ -76,7 +76,12 @@ $ih = Loader::helper('image'); //<--uncomment this line if displaying image attr
 			</h3>
 			<div class="productThumbnail"><a href="<?php echo $url ?>"><img src="<?php echo $thumbSrc ?>" width="<?php echo $thumbWidth ?>" height="<?php echo $thumbHeight ?>" alt="" /></a></div>
 			<div class="ccm-page-list-description">
-				<?php echo $description ?>...&nbsp;<a style="margin-left:-5px;margin-bottom:10px;" class="product-more-link" href="<?php echo $url ?>">more</a>
+				<?php 
+					if(strlen($description) > 200){
+						preg_replace('/\s+?(\S+)?$/', '', substr($description, 0, 201));
+					}	
+					echo $description; 
+				?>...&nbsp;<a style="margin-left:-5px;margin-bottom:10px;" class="product-more-link" href="<?php echo $url ?>">more</a>
 			</div>
 		</div>
 			
