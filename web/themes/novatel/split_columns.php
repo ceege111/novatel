c9d749d17cb0e80b1c67ca9890880d760dcdd38d
Casaval <casaval@gmail.com>
2013-01-25 14:25:47 -0800

    split column template css

--- web/themes/novatel/split_columns.php
+++ web/themes/novatel/split_columns.php
@@ -7,21 +7,20 @@ $this->inc('elements/header.php'); ?>
 	<?php $this->inc('elements/subnav.php'); ?>
 </header>
 
-<div class="container oneColumn">
+<div class="container oneColumn splitColumn">
 	<section class="column area row">
-		<div class="span6 gray-block">
+		<div class="span6 gray-block splitLeft">
 			<?php 
 			$as = new Area('Main');
 			$as->display($c);
 			?>		
 		</div>
-		<div class="span6 gray-block" style="margin-left: 10px;">
+		<div class="span6 gray-block splitRight" style="margin-left: 10px;">
 			<?php 
 			$af = new Area('Main Right');
 			$af->display($c);
 			?>
 		</div>
-
 	</section>
 
 <?php  $this->inc('elements/footer.php'); ?>
