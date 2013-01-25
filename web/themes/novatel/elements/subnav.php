85ec173865300b7bbe93353648a226236e5801c0
Casaval <casaval@gmail.com>
2013-01-25 10:30:28 -0800

    breadcrumb show on banner

--- web/themes/novatel/elements/subnav.php
+++ web/themes/novatel/elements/subnav.php
@@ -6,4 +6,10 @@ defined('C5_EXECUTE') or die("Access Denied.");
 				$ah = new Area("Sub Nav");
 				$ah->display($c);
 			?>
+		<script type="text/javascript">
+			$(document).ready(function(){
+				var offset = $(".banner").height() - 20;
+				$(".breadcrumb").css('margin-top',offset+'px').fadeIn();
+			});
+		</script>
 	</div>
