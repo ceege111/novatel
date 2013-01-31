c1b0b88acf4b4f330ff862e29b2b10496ee94f62
Casaval <casaval@gmail.com>
2013-01-31 12:13:29 -0800

    special tab menus

--- web/themes/novatel/elements/subnav.php
+++ web/themes/novatel/elements/subnav.php
@@ -10,10 +10,10 @@ defined('C5_EXECUTE') or die("Access Denied.");
 			$(document).ready(function(){
 				var breadOffset = $(".banner").find('img').first().attr('height');
 				breadOffset = parseInt(breadOffset);
-				breadOffset -= 15;
+				breadOffset -= 25;
 				console.log('breadcrumb offset: '+'-'+breadOffset+'px');
 				if ( breadOffset > 200 ){
-					$(".breadcrumb").css('margin-top','-'+breadOffset+'px').fadeIn();
+					$(".breadcrumb").css('margin-top','-'+breadOffset+'px').css('margin-left','0').fadeIn();
 				} else {
 					$(".breadcrumb").find('i').removeClass('icon-white');
 					$(".breadcrumb").addClass('default').fadeIn();
