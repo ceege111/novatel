d19785028e6440ec05f9b13e5b3255715557ccbe
Casaval <casaval@gmail.com>
2013-02-06 14:20:52 -0800

    ethergrid ajax load

--- web/themes/novatel/js/script.js
+++ web/themes/novatel/js/script.js
@@ -122,9 +122,9 @@ $(document).ready(function(){
 
 				containerSp.fadeIn(function(){
 					// $("body .wrap").css('min-height', $("body .wrap").innerHeight() );
-					if($(".ether-grid").length){
+					if($("div.ccm-gallery-wrap").length){
 						console.log('loading gridslider...');
-						$(".ether-grid").gridSlider({cols: 4, rows: 4,align:'center',width:'800px',autoplay_enable:true,autoplay_interval:6,scroll_axis:'x',loop:true,ctrl_arrows:false,ctrl_pag:true,ctrl_always_visible:true});
+						$("div.ccm-gallery-wrap").gridSlider({cols: 4, rows: 4,align:'center',width:'800px',autoplay_enable:true,autoplay_interval:6,scroll_axis:'x',loop:true,ctrl_arrows:false,ctrl_pag:true,ctrl_always_visible:true});
 					}
 				});
 			});
