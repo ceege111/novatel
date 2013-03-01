e2b740b306d6783dd8307ef52d0a815c8ba13be4
Casaval <casaval@gmail.com>
2013-03-01 09:27:00 -0800

    ajax disabled

--- web/themes/novatel/js/script.js
+++ web/themes/novatel/js/script.js
@@ -96,8 +96,8 @@ $(document).ready(function(){
 	});
 
 	//special product tabs
-	if($("html").hasClass('history')){
-	// if(0){
+	// if($("html").hasClass('history')){
+	if(0){
 		$(".nav-header-tabs-sp li").each(function(){
 			if ($("div.container.oneColumn").length) {
 				var containerSp = $("div.container.oneColumn");
