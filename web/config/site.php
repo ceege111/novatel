2479f1f0cf2394ec7b0f9b1299c1c6167465f8c0
Casaval <casaval@gmail.com>
2013-03-02 23:56:40 -0800

    cache speed up setting

--- web/config/site.php
+++ web/config/site.php
@@ -5,3 +5,4 @@ define('DB_PASSWORD', 'H8y^j0Lnen');
 define('DB_DATABASE', 'novatel');
 define('PASSWORD_SALT', 'VB6kbNLP60TfCGakVqTYaEa2T8oJaJJ0B00g1KnChhDRJnjYi8D8wfFsyemhCH31');
 define('ENABLE_NEWSFLOW_OVERLAY', FALSE);
+define('CACHE_FRONTEND_OPTIONS', serialize(array('automatic_cleaning_factor' => 0)));
