bfcccd6f4471770ebb27672f7b6ab9119f7cd8c8
novatel production server <jose@gorational.com>
2013-03-03 07:58:29 +0000

    data export from dev

--- web/config/site.php
+++ web/config/site.php
@@ -4,4 +4,4 @@ define('DB_USERNAME', 'novatel');
 define('DB_PASSWORD', 'H8y^j0Lnen');
 define('DB_DATABASE', 'novatel');
 define('PASSWORD_SALT', 'VB6kbNLP60TfCGakVqTYaEa2T8oJaJJ0B00g1KnChhDRJnjYi8D8wfFsyemhCH31');
-define('ENABLE_NEWSFLOW_OVERLAY', FALSE);
+define('ENABLE_NEWSFLOW_OVERLAY', FALSE);?><?php define('DIRNAME_APP_UPDATED', 'concrete5.6.0.2');?>
