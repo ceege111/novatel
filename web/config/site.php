d173da86c19a2ae29b7fb5af79472da734556dd5
Casaval <casaval@gmail.com>
2013-04-17 15:44:14 -0700

    put a setting in config to set the from: email to info@nvtl.com when a form is submitted

--- web/config/site.php
+++ web/config/site.php
@@ -5,6 +5,7 @@ define('DB_PASSWORD', 'H8y^j0Lnen');
 define('DB_DATABASE', 'novatel');
 define('PASSWORD_SALT', 'VB6kbNLP60TfCGakVqTYaEa2T8oJaJJ0B00g1KnChhDRJnjYi8D8wfFsyemhCH31');
 define('ENABLE_NEWSFLOW_OVERLAY', FALSE);
-
-define('FORM_BLOCK_SENDER_EMAIL', 'info@novatelwireless.com');
+define('EMAIL_DEFAULT_FROM_ADDRESS', 'info@nvtl.com');
+define('EMAIL_DEFAULT_FROM_NAME', 'NVTL.com Automated System');
+define('FORM_BLOCK_SENDER_EMAIL', EMAIL_DEFAULT_FROM_ADDRESS);
 define('CACHE_FRONTEND_OPTIONS', serialize(array('automatic_cleaning_factor' => 0)));?><?php define('DIRNAME_APP_UPDATED', 'concrete5.6.1.2_updater');?>
