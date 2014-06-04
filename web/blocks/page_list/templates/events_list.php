2caecf14a4bf8abce4efd3889c9dd883d37a517b
ajmal-farook <ajmalfarookmsm@gmail.com>
2014-06-04 21:57:16 +0530

    Update events_list.php

--- web/blocks/page_list/templates/events_list.php
+++ web/blocks/page_list/templates/events_list.php
@@ -24,9 +24,9 @@ $th = Loader::helper('text');
 		// $url = $nh->getLinkToCollection($page);
 		$target = ($page->getCollectionPointerExternalLink() != '' && $page->openCollectionPointerExternalLinkInNewWindow()) ? '_blank' : $page->getAttribute('nav_target');
 		$target = empty($target) ? '_self' : $target;
-		$description = $page->getCollectionDescription();
-		$description = $controller->truncateSummaries ? $th->shorten($description, $controller->truncateChars) : $description;
-		$description = $th->entities($description);	
+		$description = $page->getCollectionEvent();
+		//$description = $controller->truncateSummaries ? $th->shorten($description, $controller->truncateChars) : $description;
+		//$description = $th->entities($description);	
 
 		$url = ($page->getAttribute('iframe_url') != '') ? $page->getAttribute('iframe_url') : '';
 		$source = ($page->getAttribute('external_content_title') != '') ? $page->getAttribute('external_content_title') : '';
