7e8b7e027cb824dc9d0eaf78b1801a3960f1c304
developer <developer@novatel.com>
2014-09-01 10:24:59 +0000

    data export from dev

--- web/blocks/page_list/templates/events_list.php
+++ web/blocks/page_list/templates/events_list.php
@@ -26,12 +26,12 @@ $th = Loader::helper('text');
 		$target = empty($target) ? '_self' : $target;
 		$description = $page->getCollectionDescription();
 		$description = $controller->truncateSummaries ? $th->shorten($description, $controller->truncateChars) : $description;
-		//$description = $th->entities($description);	
+//		$description = $th->entities($description);	
 
 		$url = ($page->getAttribute('iframe_url') != '') ? $page->getAttribute('iframe_url') : '';
 		$source = ($page->getAttribute('external_content_title') != '') ? $page->getAttribute('external_content_title') : '';
 		$date = ($page->getAttribute('external_content_posted_date') != '') ? $page->getAttribute('external_content_posted_date') : '';
-		//$description = ($page->getAttribute('event') != '') ? $page->getAttribute('event') : '';
+
 
 		// $img = $page->getAttribute('product_image');
 		// $thumb = $ih->getThumbnail($img, 250, 9999, false);
@@ -68,7 +68,7 @@ $th = Loader::helper('text');
 		<tr><td class="row product-item">
 			<h3 class="ccm-page-list-title">
 				<?php if ($url == ''): ?>
-					<?php echo $title; ?>
+					<?php echo $title ?>
 				<?php else: ?>
 					<a href="<?php echo $url ?>" target="<?php echo $target ?>"><?php echo $title ?></a>
 				<?php endif; ?>
--- web/updates/concrete5.6.0.2/concrete/config/base.php
+++ web/updates/concrete5.6.0.2/concrete/config/base.php
@@ -114,7 +114,6 @@ define("LANGUAGE_DOMAIN_CORE", "messages");
 # Path to the core files shared between all concrete 5 installations
 if (!defined('DIR_BASE_CORE')) {
 	define('DIR_BASE_CORE', realpath(dirname(__FILE__) . '/..'));
-	
 }
 
 define('DIRNAME_CORE_CLASSES', 'core');
